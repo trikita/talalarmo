8626fbd1b9a986ef14ce7d755294e08ff3e73462
Anna Kruglik <kruglik.anna@gmail.com>
2016-03-23 20:46:17 +0800

    fixed applying app theme to the main activity

--- src/main/java/trikita/talalarmo/MainActivity.java
+++ src/main/java/trikita/talalarmo/MainActivity.java
@@ -4,21 +4,21 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import trikita.anvil.Anvil;
 import trikita.anvil.RenderableView;
 import trikita.talalarmo.ui.AlarmLayout;
+import trikita.talalarmo.ui.Theme;
 
 public class MainActivity extends Activity {
 
     @Override
     public void onCreate(Bundle b) {
         super.onCreate(b);
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
-            setTheme(android.R.style.Theme_Holo_Light_NoActionBar);
-        } else {
-            setTheme(android.R.style.Theme_Material_Light_NoActionBar);
-        }
+
+        updateTheme();
         setContentView(new RenderableView(this) {
             public void view() {
                 AlarmLayout.view();
@@ -37,6 +37,28 @@ public class MainActivity extends Activity {
     public void openSettings() {
         startActivity(new Intent(this, SettingsActivity.class));
     }
+
+    private void updateTheme() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            if (App.getState().settings().theme() == 0) {
+                setTheme(android.R.style.Theme_Holo_Light_NoActionBar);
+            } else {
+                setTheme(android.R.style.Theme_Holo_NoActionBar);
+            }
+        } else {
+            if (App.getState().settings().theme() == 0) {
+                setTheme(android.R.style.Theme_Material_Light_NoActionBar);
+            } else {
+                setTheme(android.R.style.Theme_Material_NoActionBar);
+            }
+        }
+
+        // fill statusbar with a theme dark color on post-Lollipop devices
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(Theme.get(App.getState().settings().theme()).primaryDarkColor);
         }
     }
 }
