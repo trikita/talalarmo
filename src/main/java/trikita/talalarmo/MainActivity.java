e229a5da09966b28d8e1496096d1421d6e0684e6
Anna Kruglik <kruglik.anna@gmail.com>
2016-03-23 20:40:11 +0800

    removed obsolete code handling result from the ringtone picker; removed unused imports

--- src/main/java/trikita/talalarmo/MainActivity.java
+++ src/main/java/trikita/talalarmo/MainActivity.java
@@ -2,22 +2,15 @@ package trikita.talalarmo;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.media.RingtoneManager;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 
 import trikita.anvil.Anvil;
 import trikita.anvil.RenderableView;
-import trikita.jedux.Action;
-import trikita.jedux.Logger;
-import trikita.jedux.Store;
 import trikita.talalarmo.ui.AlarmLayout;
 import trikita.talalarmo.ui.SettingsLayout;
 
 public class MainActivity extends Activity {
-
-    public static final int RINGTONE_REQUEST_CODE = 100;
 
     @Override
     public void onCreate(Bundle b) {
@@ -38,7 +31,6 @@ public class MainActivity extends Activity {
         });
     }
 
-    @Override
     public void onBackPressed() {
         App.dispatch(new Action<>(Actions.Navigation.BACK));
         if (App.getState().navigation().exit()) {
@@ -47,12 +39,6 @@ public class MainActivity extends Activity {
         }
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == RINGTONE_REQUEST_CODE && data != null) {
-            Uri uri = data.getParcelableExtra(RingtoneManager.EXTRA_RINGTONE_PICKED_URI);
-            App.dispatch(new Action<>(Actions.Settings.SET_RINGTONE, uri.toString()));
     public void openSettings() {
         startActivity(new Intent(this, SettingsActivity.class));
     }
